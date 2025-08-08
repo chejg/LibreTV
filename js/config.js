8edda0414787dd4d72e0d64d94703808d10bc5dc
chejg <33821723+chejg@users.noreply.github.com>
2025-08-09 00:10:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,15 +110,57 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+	     sl: {
+         api: 'https://slapibf.com',
+         name: '森林1资源',
+         adult: true
+     },
+     jkun: {
+         api: 'https://jkunzyapi.com/api.php/provide/vod',
+         name: 'jkun资源',
+         adult: true
+     },
+     th: {
+         api: 'https://thzy1.me/api.php/provide/vod',
+         name: '桃花资源',
+         adult: true
+     },
+     souav: {
+         api: 'https://api.souavzy.vip/api.php/provide/vod',
+         name: 'souav资源',
+         adult: true
+     },
+     aosika: {
+         api: 'https://askjxbf.com/api.php/provide/vod',
+         name: '奥斯卡资源',
+         adult: true
+     },
+     lsb: {
+         api: 'https://apilsbzy1.com/api.php/provide/vod',
+         name: 'lsb资源',
+         adult: true
+     },
+    bwzy: {
+         api: 'https://api.bwzym3u8.com/api.php/provide/vod',
+         name: 'bwzy',
+         adult: true,
+     },
+     huangcang: {
+         api: 'https://hsckzy888.com/api.php/provide/vod',
+         name: '黄色仓库',
+         adult: true,
+         detail: 'https://hsckzy888.com/api.php/provide/vod'
+     },
+     yutu: {
+         api: 'https://yutuzy10.com/api.php/provide/vod',
+         name: '玉兔资源',
+         adult: true
+     },
     lzi: {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    },
+
 };
 
 // 定义合并方法
